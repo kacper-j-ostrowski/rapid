32e18bf169a0e6beb282c26bed9f9e5b8c5ac82e
kacperjostrowski@gmail.com <kacperjostrowski@gmail.com>
2018-04-25 19:20:04 +0200

    init

---
+++ src/main/java/pl/ostrowski/rapid/RapidApplication.java
@@ -0,0 +1,12 @@
+package pl.ostrowski.rapid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidApplication.class, args);
+	}
+}
